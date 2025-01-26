283d538670fa463b8649d4f7ed4a5eb6c1a202d8
ericflores108 <eflorty108@gmail.com>
2025-01-25 20:53:30 -0800

    fix: remove server package

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/ericflores108/spotify/api"
 	"github.com/ericflores108/spotify/config"
 	"github.com/ericflores108/spotify/logger"
-	"github.com/ericflores108/spotify/server"
 	"github.com/ericflores108/spotify/service"
 )
 
@@ -29,7 +29,7 @@ func main() {
 	)
 
 	// Initialize the server and register routes
-	srv := server.NewServer(ctx, svc)
+	srv := api.NewServer(ctx, svc)
 	mux := srv.RegisterRoutes()
 
 	// Determine port for HTTP service
